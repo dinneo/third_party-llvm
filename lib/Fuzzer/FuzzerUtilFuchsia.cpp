b6ee42107bb145506aa8a44f639fd079f0a22aa1
Aaron Green <aarongreen@google.com>
2017-07-14 15:29:16 -0700

    [lib/Fuzzer] Initial commit.
    Change-Id: I63524faa185f3297ca195d2c0b975842192e5f90

---
+++ lib/Fuzzer/FuzzerUtilFuchsia.cpp
@@ -0,0 +1,13 @@
+//===- FuzzerUtilFuchsia.cpp - Misc utils ---------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+// Misc utils for Fuchsia.
+//===----------------------------------------------------------------------===//
+#include "FuzzerDefs.h"
+#if LIBFUZZER_FUCHSIA
+#endif // LIBFUZZER_LINUX
